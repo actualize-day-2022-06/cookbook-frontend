7ceba9b6c885b40589c3e5a9f104182e4ab3074f
brian <riceb53@gmail.com>
2022-08-22 09:26:11 -0700

    add boilerplate

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      name: "brian"
+    };
+  },
+  created: function() {},
+  methods: {}
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <h1>{{ name }}</h1>
+    <button>new button</button>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style></style>
