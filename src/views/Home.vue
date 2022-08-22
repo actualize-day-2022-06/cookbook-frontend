e2b3f3afe89bf01d7e7e57e2237dbe067504d160
brian <riceb53@gmail.com>
2022-08-22 11:06:29 -0700

    add hardcoded create

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,6 +18,22 @@ export default {
         console.log(response.data);
         this.recipes = response.data
       })
+    },
+    createRecipe: function() {
+      console.log('creating recipe...');
+      // get data from the user
+      // send that data to rails
+      var params = {
+        title: "BEST salad",
+        ingredients: "lettuce, tomatoes, bacon",
+        directions: "mix ingredients together",
+        prep_time: 5,
+        chef: "brian",        
+      }
+      axios.post("http://localhost:3000/recipes.json", params).then(response => {
+        console.log(response.data);
+        this.recipes.push(response.data);
+      })
     }
   }
 };
@@ -26,12 +42,24 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <h1>{{ name }}</h1>
+    <hr />
+    <hr />
+    <button v-on:click="createRecipe()">Make new recipe</button>
+    <hr />
+    <hr />
+
+
+
+
     <!-- <h1>{{ recipes }}</h1> -->
     <div v-for="recipe in recipes" v-bind:key="recipe.id">
       <p>{{ recipe.title }}</p>
       <p>{{ recipe.directions }}</p>
       <p>{{ recipe.image_url }}</p>
+
+      <!-- v-on:click == @click
+      v-bind:src == :src -->
+      
       <img v-bind:src="recipe.image_url" />
       <br />
       <br />
